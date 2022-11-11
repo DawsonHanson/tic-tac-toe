5975bc2b023b16c6e9151e81f1ad9cf4063c17c2
Dawson Hanson <Dawson.Hanson2@gmail.com>
2022-11-11 15:18:42 -0700

    Changed function for adding players

--- javascript.js
+++ javascript.js
@@ -1,51 +1,49 @@
 console.log('Test!');
 
-const playerFactory = (name) => {
-  return {name}
-};
+const playerModule = (function() {
 
-(function() {
+  let players = []
 
-  let test = {
-    players: [],
-
-    init: function() {
-      this.cacheDom()
-      this.bindEvents()
-    },
-
-    cacheDom: function() {
-      button1 = document.getElementById('button1')
-      button2 = document.getElementById('button2')
-      player1Input = document.getElementById('player1')
-      player2Input = document.getElementById('player2')
-      display = document.getElementById('display')
-    },
-
-    bindEvents: function() {
-      button1.addEventListener('click', this.addPlayer1.bind(this))
-      button2.addEventListener('click', this.addPlayer2.bind(this))
-    },
-
-    addPlayer1: function() {
-      this.players.push(playerFactory(player1Input.value))
-      this.render(`Welcome ${player1Input.value}!`)
-      console.log(this.players)
-    },
-
-    addPlayer2: function() {
-      this.players.push(playerFactory(player2Input.value))
-      this.render(`Welcome ${player2Input.value}!`)
-      console.log(this.players)
-    },
-
-    render: function(text) {
-      display.textContent = text
-    },
-
+  const _playerFactory = (name, value) => {
+    return {name, value}
   };
 
-  test.init();
+  const init = function() {
+    _cacheDom()
+    _bindEvents()
+  }
 
-})()
+  const _cacheDom = function() {
+    button1 = document.getElementById('button1')
+    button2 = document.getElementById('button2')
+    player1Input = document.getElementById('player1')
+    player2Input = document.getElementById('player2')
+    display = document.getElementById('display')
+  }
 
+  const _bindEvents = function() {
+    button1.addEventListener('click', _addPlayer1)
+    button2.addEventListener('click', _addPlayer2)
+  }
+
+  const _addPlayer1 = function() {
+    players.push(_playerFactory(player1Input.value, 1))
+    _render(`Welcome ${player1Input.value}!`)
+    console.log(playerModule.players)
+  }
+
+  const _addPlayer2 = function() {
+    players.push(_playerFactory(player2Input.value, 2))
+    _render(`Welcome ${player2Input.value}!`)
+    console.log(playerModule.players)
+  }
+
+  const _render = function(text) {
+    display.textContent = text
+  }
+
+  return {init, players}
+
+})();
+
+playerModule.init()
